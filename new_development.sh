b92a3475068dc3439551e81d2b62da10ced83360
chalapathi <72649121+chalapathi-dba@users.noreply.github.com>
2026-01-05 15:14:38 +0530

    adding a new development with new_development.sh script
    adding a new development with new_development.sh script

---
+++ new_development.sh
@@ -0,0 +1,5 @@
+echo "adding a new development with new_development.sh script"
+echo " after this we will send to QA"
+echo "then will merge to main prod brnach"
+
+echo "development completed. Moving to QA"
